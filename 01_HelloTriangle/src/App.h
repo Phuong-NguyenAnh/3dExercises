78a38452bb9f3d72218bb214d7fc024611ba7cd5
Phương Nguyễn <phuongna1908@gmail.com>
2019-06-22 15:22:20 +0700

    Set different color red/green/blue for each vertex to see a colorful triangle

--- 01_HelloTriangle/src/App.h
+++ 01_HelloTriangle/src/App.h
@@ -17,8 +17,11 @@ public:
 	{
 		const std::string vsSource = "\
 			attribute vec2 a_position;\
+			attribute vec4 a_color;\
+			varying vec4 v_color;\
 			void main()\
 			{\
+				v_color = a_color;\
 				gl_Position = vec4(a_position, 0.0, 1.0);\
 			}";
 		auto vs = Utils::compileShader(vsSource, GL_VERTEX_SHADER);
@@ -26,9 +29,10 @@ public:
 
 		const std::string fsSource = "\
 			precision mediump float;\
+			varying vec4 v_color;\
 			void main()\
 			{\
-				gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);\
+				gl_FragColor = v_color;\
 			}";
 		auto fs = Utils::compileShader(fsSource, GL_FRAGMENT_SHADER);
 		assert(fs > 0);
@@ -38,14 +42,24 @@ public:
 		glUseProgram(program);
 
 		static float positions[] = {
-			0.f, 1.f,
-			-1.f, -1.f,
-			1.f, -1.f
+			0.f, .5f,
+			-.5f, -.5f,
+			.5f, -.5f
 		};
 		auto positionLocation = glGetAttribLocation(program, "a_position");
 		assert(positionLocation >= 0);
 		glVertexAttribPointer(positionLocation, 2, GL_FLOAT, GL_FALSE, 0, positions);
 		glEnableVertexAttribArray(positionLocation);
+
+		static float colors[] = {
+			1.f, 0.f, 0.f, 1.f,
+			0.f, 1.f, 0.f, 1.f,
+			0.f, 0.f, 1.f, 1.f,
+		};
+		auto colorLocation = glGetAttribLocation(program, "a_color");
+		assert(colorLocation >= 0);
+		glVertexAttribPointer(colorLocation, 4, GL_FLOAT, GL_FALSE, 0, colors);
+		glEnableVertexAttribArray(colorLocation);
 
 		glClearColor(0.f, 0.f, 0.f, 1.f);
 	}
